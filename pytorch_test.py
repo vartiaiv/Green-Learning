d1bff3f2031d58559d1347e739f7450dc6fc588f
Ville Vartiainen <villev99@gmail.com>
2023-02-01 19:29:58 +0200

    Added gitignore and commented code

--- pytorch_test.py
+++ pytorch_test.py
@@ -2,27 +2,27 @@
 import numpy as np
 import math
 import torch
-from thop import profile
+
+from thop import profile  # used to calculate MACs (Roughly MAC = 0.5 * FLOP)
+# MACs = multiply–accumulate operations
+# FLOPs = floating-point operations per second
+from thop import clever_format
+
 
 # setting device on GPU if available, else CPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
 print()
 
-#Additional Info when using cuda
+# Additional Info when using cuda
 if device.type == 'cuda':
     print(torch.cuda.get_device_name(0))
     print('Memory Usage:')
-    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    # The following should be low before run
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')  
     print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
 
-### Move tensors to the respective device
-# torch.rand(10).to(device)
-
-### Create tensor directly on the device
-# torch.rand(10, device=device)
-
-def test1():
+def numpyTest():
     # Create random input and output data
     x = np.linspace(-math.pi, math.pi, 2000)
     y = np.sin(x)
@@ -60,7 +60,7 @@ def test1():
     print(f'Result: y = {a} + {b} x + {c} x^2 + {d} x^3')
 
 
-def test2():
+def tensorsTest():
     dtype = torch.float
 
     # Create random input and output data
@@ -99,31 +99,27 @@ def test2():
 
     print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
 
-def test3():
-    # Create Tensors to hold input and outputs.
-    x = torch.linspace(-math.pi, math.pi, 2000)
+
+def tensorAutogradTest():
+    # Create Tensors to hold input and outputs. 
+    # Given device=<device> they are created directly on <device> whereas 
+    # x.to(<device>) would move them to the <device>
+    LEN = 2000
+    x = torch.linspace(-math.pi, math.pi, LEN, device=device)
     y = torch.sin(x)
 
-    # For this example, the output y is a linear function of (x, x^2, x^3), so
-    # we can consider it as a linear layer neural network. Let's prepare the
-    # tensor (x, x^2, x^3).
-    p = torch.tensor([1, 2, 3])
-    xx = x.unsqueeze(-1).pow(p)
-
-    # In the above code, x.unsqueeze(-1) has shape (2000, 1), and p has shape
+    p = torch.tensor([1, 2, 3], device=device)
+    xx = x.unsqueeze(-1).pow(p).to(device)
+    # In the above code, x.unsqueeze(-1) has shape (LEN, 1), and p has shape
     # (3,), for this case, broadcasting semantics will apply to obtain a tensor
-    # of shape (2000, 3) 
+    # of shape (LEN, 3) 
 
-    # Use the nn package to define our model as a sequence of layers. nn.Sequential
-    # is a Module which contains other Modules, and applies them in sequence to
-    # produce its output. The Linear Module computes output from input using a
-    # linear function, and holds internal Tensors for its weight and bias.
-    # The Flatten layer flatens the output of the linear layer to a 1D tensor,
+    # The Flatten layer flattens the output of the linear layer to a 1D tensor,
     # to match the shape of `y`.
     model = torch.nn.Sequential(
         torch.nn.Linear(3, 1),
         torch.nn.Flatten(0, 1)
-    )
+    ).to(device)
 
     macs, params = profile(model, inputs=(xx, ))
 
@@ -132,28 +128,24 @@ def test3():
     loss_fn = torch.nn.MSELoss(reduction='sum')
 
     learning_rate = 1e-6
-    for t in range(2000):
+    for t in range(LEN):
 
-        # Forward pass: compute predicted y by passing x to the model. Module objects
-        # override the __call__ operator so you can call them like functions. When
-        # doing so you pass a Tensor of input data to the Module and it produces
-        # a Tensor of output data.
+        # Forward pass: compute predicted y by passing x to the model. 
         y_pred = model(xx)
 
         # Compute and print loss. We pass Tensors containing the predicted and true
         # values of y, and the loss function returns a Tensor containing the
         # loss.
         loss = loss_fn(y_pred, y)
-        if t % 100 == 99:
+        LEN_10_p = int(round(LEN*0.1))
+        if t % (LEN_10_p) == LEN_10_p-1:
             print(t, loss.item())
 
         # Zero the gradients before running the backward pass.
         model.zero_grad()
 
         # Backward pass: compute gradient of the loss with respect to all the learnable
-        # parameters of the model. Internally, the parameters of each Module are stored
-        # in Tensors with requires_grad=True, so this call will compute gradients for
-        # all learnable parameters in the model.
+        # parameters of the model.
         loss.backward()
 
         # Update the weights using gradient descent. Each parameter is a Tensor, so
@@ -167,3 +159,10 @@ def test3():
 
     # For linear layer, its parameters are stored as `weight` and `bias`.
     print(f'Result: y = {linear_layer.bias.item()} + {linear_layer.weight[:, 0].item()} x + {linear_layer.weight[:, 1].item()} x^2 + {linear_layer.weight[:, 2].item()} x^3')
+    
+    # Format and print thop profile
+    macs, params = clever_format([macs, params], "%.3f")  
+    print(f"With params {params}, MACs: {macs}")
+
+if __name__ == "__main__":
+    tensorAutogradTest()
